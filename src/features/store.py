a3c0f4f20dca6933a04685b7279d549264d205bf
vovanbart <vovanbart@icloud.com>
2022-12-28 11:57:31 +0300

    sending email

--- src/features/store.py
+++ src/features/store.py
@@ -3,7 +3,8 @@
 # import smtplib
 
 
-# Нужно поменять message['From'] на адрес реальной почты проекта, в password записать пароль от нее
+# Нужно поменять message['From'] на адрес реальной почты проекта, 
+# в password записать пароль от нее
 # также в строке 16 указать сервер почты для smtp(например smtp.yandex.ru)
 # def send_mail(user_email):
 #     message = MIMEMultipart()
