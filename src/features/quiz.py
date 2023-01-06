f3926e4fa9bf0be5cd84a6859b94ce53870e7505
Никита Сорин <nikitasorin@Nikita-MacBook.local>
2023-01-06 14:43:08 +0300

    #14 fix flake8

--- src/features/quiz.py
+++ src/features/quiz.py
@@ -68,21 +68,17 @@ def quiz(update=None, context=None, chat_id=None, index=None):
 def analize_results(final_points):
     """Функция для анализа результата ответов пользователя на вопросы."""
     if final_points > 9:
-        text = f'Количество правильных ответов: {final_points}, \
+        return f'Количество правильных ответов: {final_points}, \
 твой статус "Хоккейный гуру"'
-        return text
     elif final_points <= 8 and final_points >= 7:
-        text = f'Количество правильных ответов: {final_points}, \
+        return f'Количество правильных ответов: {final_points}, \
 твой статус "Знаток хоккея"'
-        return text
     elif final_points <= 6 and final_points > 5:
-        text = f'Количество правильных ответов: {final_points}, \
+        return f'Количество правильных ответов: {final_points}, \
 твой статус "Хоккейный профи"'
-        return text
     elif final_points < 5:
-        text = f'Количество правильных ответов: {final_points}, \
+        return f'Количество правильных ответов: {final_points}, \
 ты молодец, но еще нужно подтянуть знания!'
-        return text
 
 
 def poll_handler(update, context):
