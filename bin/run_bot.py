e5b87cb33a829291522f576135c2ad7912a96cf6
MrGorkiy <mbrysin@bk.ru>
2023-03-10 11:03:05 +0300

    FIX isort

--- bin/run_bot.py
+++ bin/run_bot.py
@@ -1,6 +1,6 @@
-from src.features.base import show_main_menu
 from prometheus_client import start_http_server
 
+from src.features.base import show_main_menu
 
 if __name__ == '__main__':
     start_http_server(8000)
