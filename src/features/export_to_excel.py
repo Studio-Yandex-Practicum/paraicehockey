c4c8187c066ee7b44727c813c3d19dbf2fee5649
AndreiUkladchikov <73438327+AndreiUkladchikov@users.noreply.github.com>
2023-03-16 20:04:52 +0400

    Update export_to_excel.py

--- src/features/export_to_excel.py
+++ src/features/export_to_excel.py
@@ -19,7 +19,7 @@ def get_metrics_list():
         try:
             response = requests.get(url)
         except requests.exceptions.RequestException:
-            logging.error('Prometheus: can't get metrics: u=%url', url)
+            logging.error('Prometheus: cant get metrics: u=%url', url)
         data = response.json()['data']
         for metric in data:
             name = metric['__name__']
