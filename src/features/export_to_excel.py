3dc0b9773d2f7497743bf39c1f7b1fe0d20f622c
MrGorkiy <mbrysin@bk.ru>
2023-03-31 09:44:40 +0300

    FIX Незначительные изменения для теста

--- src/features/export_to_excel.py
+++ src/features/export_to_excel.py
@@ -21,7 +21,7 @@ def get_metrics_list():
         try:
             response = requests.get(url)
         except requests.exceptions.RequestException:
-            logging.error('Prometheus: не удалось получить метрику.')
+            logging.error('Prometheus: не удалось получить метрику. ')
         data = response.json()['data']
         for metric in data:
             name = metric['__name__']
